dcb18c5ddffecc64657f9168827742dcda887e12
Caio Lucas <lcaio1281@gmail.com>
2021-05-13 16:27:46 -0300

    added global.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6;
-`
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div className="App">
-      <Title>
-        Hello world
-      </Title>
+      <h1>Hello world</h1>
+      <GlobalStyle />
     </div>
   );
 }
