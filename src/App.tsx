9a251c25409a7771ed4bde3c82d99b28c97fb059
Caio Lucas <lcaio1281@gmail.com>
2021-05-13 00:54:27 -0300

    using styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size: 64px;
+  color: #8257e6;
+`
+
 export function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Title>
+        Hello world
+      </Title>
     </div>
   );
 }
