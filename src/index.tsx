bd44817bd4bba2066abd8cebd45c734c203deed5
Caio Lucas <lcaio1281@gmail.com>
2021-05-20 01:16:21 -0300

    listando transações e seeds

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,30 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    // simulando dados pré-cadastrados
+    server.db.loadData({
+      transactions: [ // smp nome do model no plural
+        {
+          id: 1,
+          title: 'Freelancer de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-02-14 11:00:00'),
+        },
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
