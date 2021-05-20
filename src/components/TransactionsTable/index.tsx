13475255872fa94bca181c6d662ec071569c2a7d
Caio Lucas <lcaio1281@gmail.com>
2021-05-20 01:24:07 -0300

    Formatando valores c/ Intl

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { api } from "../../services/api";
 import { Container } from "./styles"
 
-interface Transaction{
+interface Transaction {
     id: number;
     title: string;
     amount: number;
@@ -36,9 +36,18 @@ export function TransactionsTable() {
                         return (
                             <tr key={transaction.id}>
                                 <td>{transaction.title}</td>
-                                <td className={transaction.type}>{transaction.amount}</td>
+                                <td className={transaction.type}>
+                                    {new Intl.NumberFormat('pt-BR', {
+                                        style: 'currency',
+                                        currency: 'BRL'
+                                    }).format(transaction.amount)}
+                                </td>
                                 <td>{transaction.category}</td>
-                                <td>{transaction.createdAt}</td>
+                                <td>
+                                    {new Intl.DateTimeFormat('pt-BR').format(
+                                        new Date(transaction.createdAt) // convertendo string em Date 
+                                    )}
+                                </td>
                             </tr>
                         );
                     })}
