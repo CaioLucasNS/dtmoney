668544bcdf75fb5b9f5c4fc836db9a258f91f99e
Caio Lucas <lcaio1281@gmail.com>
2021-05-20 00:56:49 -0300

    put transactions in mirage api

--- src/components/NewTransactionsModal/index.tsx
+++ src/components/NewTransactionsModal/index.tsx
@@ -4,6 +4,8 @@ import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg'
 
+import { api } from '../../services/api';
+
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
 
 interface NewTransactionModalProps {
@@ -21,12 +23,14 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         // prevenindo p fechamento após o submit
         event.preventDefault();
 
-        console.log({
+        const data = {
             title, 
             value,
             category,
             type,
-        })
+        };
+
+        api.post('/transactions', data)
     }
 
     return (
